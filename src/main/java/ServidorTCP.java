3dd2215a757a1849a3b5f382c89b23733b081390
ditorre <delatorre.g.diego@gmail.com>
2023-05-11 13:58:59 +0200

    final

--- src/main/java/ServidorTCP.java
+++ src/main/java/ServidorTCP.java
@@ -13,11 +13,29 @@ public class ServidorTCP {
             Socket s = ss.accept();
             PrintWriter salida = new PrintWriter(s.getOutputStream());
             salida.println(mensaje);
+            salida.flush();
             s.close();
             ss.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void servidorTablaMultiplicar(int puerto){
+        try {
+            ServerSocket ss = new ServerSocket(puerto);
+            Socket s = ss.accept();
+            PrintWriter salida = new PrintWriter(s.getOutputStream());
+            Scanner entrada = new Scanner(s.getInputStream());
+            int aux = entrada.nextInt();
+            for(int i=0;i<10;i++){
+                salida.println(aux + " * " + i + " = =" + aux*i);
+            }
+            salida.flush();
+            s.close();
+            ss.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
