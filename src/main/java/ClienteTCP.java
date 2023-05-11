91c3a49ad6bd5b8b014a1513b1368cdb8f6e9061
ditorre <delatorre.g.diego@gmail.com>
2023-05-11 13:40:26 +0200

    a

--- src/main/java/ClienteTCP.java
+++ src/main/java/ClienteTCP.java
@@ -6,7 +6,9 @@ public class ClienteTCP {
     public static void main(String[] args){
         //pruebaCliente("zoltar.redes.upv.es",21);
         //pruebaCliente("test.rebex.net",21);
-        getPaginaWeb("www.upv.es",80);
+        //getPaginaWeb("www.upv.es",80);
+        //pruebaCliente("djxmmx.net",17);
+        mandarMensaje("10.11.69.1",55555,"Diego de la Torre Gutiérrez");
     }
 
     public static void pruebaCliente(String nombre, int puerto){
@@ -15,7 +17,7 @@ public class ClienteTCP {
             Scanner entrada = new Scanner(s.getInputStream());
             System.out.println(entrada.nextLine());
             s.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -28,9 +30,24 @@ public class ClienteTCP {
             salida.print("GET /es/ HTTP/1.0" + "\r\n");
             salida.print("\r\n");
             salida.flush();
-            for(int i=0; i < 20; i++){
+            while(entrada.hasNext()){
                 System.out.println(entrada.nextLine());
             }
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void mandarMensaje(String nombre, int puerto, String mensaje){
+        try {
+            Socket s = new Socket(nombre,puerto);
+            Scanner entrada = new Scanner(s.getInputStream());
+            PrintWriter salida = new PrintWriter(s.getOutputStream());
+            salida.print(mensaje + "\r\n");
+            salida.flush();
+            System.out.println(entrada.nextLine());
             s.close();
         } catch (IOException e) {
             e.printStackTrace();
